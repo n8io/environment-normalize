ce1918859ba9086e8a59ea5e9ab3c399776b5787
Nate Clark <nate.clark@twintechs.com>
2016-11-17 14:58:58 -0500

    Add min vs non-min'd output

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,22 @@
-const libraryName = 'environment-normalize';
+import fs from 'fs';
+import path from 'path';
+import webpack from 'webpack';
+
+const plugins = [];
+const pkgJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const {name: libraryName} = pkgJson;
+const isMinify = !!process.env.MINIFY;
+
+if (isMinify) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
+}
+
 const config = {
   entry: './src/index.js',
   devtool: 'inline-source-map',
   output: {
     path: `${__dirname}/lib`,
-    filename: `${libraryName}.js`,
+    filename: `${libraryName}${isMinify ? '.min' : ''}.js`,
     library: libraryName,
     libraryTarget: 'umd',
     umdNamedDefine: true
@@ -13,10 +25,16 @@ const config = {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel'
+        loader: 'babel',
+        exclude: /(node_modules)/
       }
     ]
-  }
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js']
+  },
+  plugins: plugins
 };
 
 export default config;
